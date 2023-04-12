0f7538b277581a703bea6941d4fc1bb7dd983184
Grush-cloud <georgeashinze@gmail.com>
2023-04-12 09:16:40 +0100

    init vite project

---
+++ src/routes/Root.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import "../App.css";
+
+export default function Root() {
+  return (
+    <>
+      <nav className="">
+        <ul className="NavBar">
+          <li>
+            <Link to="/" className="nav-links">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about-us" className="nav-links">
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link to="/listings" className="nav-links">
+              Listings
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="nav-links">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link to="/list-apartment" className="nav-links">
+              List Apartment
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  );
+}
